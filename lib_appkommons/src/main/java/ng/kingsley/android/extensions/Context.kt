5895bc21d49e886a7ec6ea2e39e833bee11ce4b4
Kingsley Adio <adiksonline@gmail.com>
2016-07-02 14:08:02 +0100

    Add Context.drawable extension to ease drawable loading, vectors included

--- lib_appkommons/src/main/java/ng/kingsley/android/extensions/Context.kt
+++ lib_appkommons/src/main/java/ng/kingsley/android/extensions/Context.kt
@@ -1,8 +1,12 @@
 package ng.kingsley.android.extensions
 
 import android.content.Context
+import android.content.res.Resources
+import android.graphics.drawable.Drawable
 import android.support.annotation.ColorRes
+import android.support.annotation.DrawableRes
 import android.support.v4.content.ContextCompat
+import android.support.v4.content.res.ResourcesCompat
 
 /**
  * @author ADIO Kingsley O.
@@ -11,3 +15,5 @@ import android.support.v4.content.ContextCompat
 
 
 fun Context.color(@ColorRes res: Int): Int = ContextCompat.getColor(this, res)
+
+fun Context.drawable(@DrawableRes res: Int, theme: Resources.Theme? = null): Drawable? = ResourcesCompat.getDrawable(resources, res, theme)
