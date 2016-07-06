58be144267696faeea053cfae5d2d90ed81365f6
Kingsley Adio <adiksonline@gmail.com>
2016-07-06 23:33:32 +0100

    Add drawable and tintedDrawable extensions to Context to ease loading (vector) drawables

--- lib_appkommons/src/main/java/ng/kingsley/android/extensions/Context.kt
+++ lib_appkommons/src/main/java/ng/kingsley/android/extensions/Context.kt
@@ -7,6 +7,7 @@ import android.support.annotation.ColorRes
 import android.support.annotation.DrawableRes
 import android.support.v4.content.ContextCompat
 import android.support.v4.content.res.ResourcesCompat
+import android.support.v4.graphics.drawable.DrawableCompat
 
 /**
  * @author ADIO Kingsley O.
@@ -16,4 +17,12 @@ import android.support.v4.content.res.ResourcesCompat
 
 fun Context.color(@ColorRes res: Int): Int = ContextCompat.getColor(this, res)
 
-fun Context.drawable(@DrawableRes res: Int, theme: Resources.Theme? = null): Drawable? = ResourcesCompat.getDrawable(resources, res, theme)
+fun Context.drawable(@DrawableRes res: Int, theme: Resources.Theme? = null): Drawable? {
+    return ResourcesCompat.getDrawable(resources, res, theme)
+}
+
+fun Context.tintedDrawable(@DrawableRes res: Int, tint: Int, theme: Resources.Theme? = null): Drawable? {
+    return drawable(res, theme)?.mutate()?.apply {
+        DrawableCompat.setTint(DrawableCompat.wrap(this), tint)
+    }
+}
