2acd22c353acccae1412f423f385cb967d123250
Kingsley Adio <adiksonline@gmail.com>
2017-02-13 13:01:04 +0100

    Review Log utility class. Expose methods to tune minimum log level

--- appcommons/src/main/java/ng/kingsley/android/util/Log.java
+++ appcommons/src/main/java/ng/kingsley/android/util/Log.java
@@ -1,8 +1,5 @@
 package ng.kingsley.android.util;
 
-
-import ng.kingsley.android.appcommons.BuildConfig;
-
 /**
  * @author ADIO Kingsley O.
  * @since 28 Jul, 2015
@@ -12,6 +9,20 @@ public final class Log {
     private static final String LOG_PREFIX = "";
     private static final int LOG_PREFIX_LENGTH = LOG_PREFIX.length();
     private static final int MAX_LOG_TAG_LENGTH = 23;
+
+    private static int logLevel = android.util.Log.DEBUG;
+
+    public static int getLogLevel() {
+        return logLevel;
+    }
+
+    public static void setLogLevel(int logLevel) {
+        Log.logLevel = logLevel;
+    }
+
+    private static boolean shouldLog(int logLevel) {
+        return logLevel >= Log.logLevel;
+    }
 
     public static String makeLogTag(String str) {
         if (str.length() > MAX_LOG_TAG_LENGTH - LOG_PREFIX_LENGTH) {
@@ -26,58 +37,50 @@ public final class Log {
     }
 
     public static void v(final String tag, String message) {
-        if (BuildConfig.DEBUG && android.util.Log.isLoggable(tag, android.util.Log.VERBOSE)) {
-            android.util.Log.v(tag, message);
-        }
+        if (shouldLog(android.util.Log.VERBOSE)) android.util.Log.v(tag, message);
     }
 
     public static void v(final String tag, String message, Throwable cause) {
-        if (BuildConfig.DEBUG && android.util.Log.isLoggable(tag, android.util.Log.VERBOSE)) {
-            android.util.Log.v(tag, message, cause);
-        }
+        if (shouldLog(android.util.Log.VERBOSE)) android.util.Log.v(tag, message, cause);
     }
 
     public static void d(final String tag, String message) {
-        if (BuildConfig.DEBUG || android.util.Log.isLoggable(tag, android.util.Log.DEBUG)) {
-            android.util.Log.d(tag, message);
-        }
+        if (shouldLog(android.util.Log.DEBUG)) android.util.Log.d(tag, message);
     }
 
     public static void d(final String tag, String message, Throwable cause) {
-        if (BuildConfig.DEBUG || android.util.Log.isLoggable(tag, android.util.Log.DEBUG)) {
-            android.util.Log.d(tag, message, cause);
-        }
+        if (shouldLog(android.util.Log.DEBUG)) android.util.Log.d(tag, message, cause);
     }
 
     public static void i(final String tag, String message) {
-        android.util.Log.i(tag, message);
+        if (shouldLog(android.util.Log.INFO)) android.util.Log.i(tag, message);
     }
 
     public static void i(final String tag, String message, Throwable cause) {
-        android.util.Log.i(tag, message, cause);
+        if (shouldLog(android.util.Log.INFO)) android.util.Log.i(tag, message, cause);
     }
 
     public static void w(final String tag, String message) {
-        android.util.Log.w(tag, message);
+        if (shouldLog(android.util.Log.WARN)) android.util.Log.w(tag, message);
     }
 
     public static void w(final String tag, String message, Throwable cause) {
-        android.util.Log.w(tag, message, cause);
+        if (shouldLog(android.util.Log.WARN)) android.util.Log.w(tag, message, cause);
     }
 
     public static void e(final String tag, String message) {
-        android.util.Log.e(tag, message);
+        if (shouldLog(android.util.Log.ERROR)) android.util.Log.e(tag, message);
     }
 
     public static void e(final String tag, String message, Throwable cause) {
-        android.util.Log.e(tag, message, cause);
+        if (shouldLog(android.util.Log.ERROR)) android.util.Log.e(tag, message, cause);
     }
 
     public static void wtf(final String tag, String message) {
-        android.util.Log.wtf(tag, message);
+        if (shouldLog(android.util.Log.ERROR)) android.util.Log.wtf(tag, message);
     }
 
     public static void wtf(final String tag, String message, Throwable cause) {
-        android.util.Log.wtf(tag, message, cause);
+        if (shouldLog(android.util.Log.ERROR)) android.util.Log.wtf(tag, message, cause);
     }
 }
