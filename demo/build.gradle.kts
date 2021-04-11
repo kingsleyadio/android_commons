684147ee1e1ac2909f5becf85e5016c0118b053a
Kingsley Adio <adiksonline@gmail.com>
2021-04-11 19:29:34 +0200

    Update package references in demo module and migrate to view binding

---
+++ demo/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    id("com.android.application")
+    kotlin("android")
+}
+
+android {
+    defaultConfig {
+        applicationId = "com.kingsleyadio.appcommons.demo"
+        versionCode = 1
+        versionName = "1.0"
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+    implementation(project(":bootstrap"))
+    implementation(project(":view"))
+    val kotlinVersion: String by rootProject
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion")
+    val googleMaterial: String by rootProject
+    implementation("com.google.android.material:material:$googleMaterial")
+
+    testImplementation(kotlin("test-junit"))
+}
